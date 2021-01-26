658fa314586d10a3d0eb0d2995d01456c690af46
Sebastian Troitzsch <s@troitzs.ch>
2021-01-26 14:29:11 +0800

    step_2: Add out-of-sample problem

--- scripts/step_2.py
+++ scripts/step_2.py
@@ -176,6 +176,8 @@ def main():
     )
     # Append time step to match length of scenario time horizon.
     irradiation_out_of_sample.loc['24:00', :] = 0.0
+    # Obtain scenario index short-hand.
+    out_of_sample_scenarios = irradiation_out_of_sample.columns
 
     # Obtain in-sample scenarios.
     # - Select representative scenarios by time series clustering.
@@ -529,6 +531,345 @@ def main():
     in_sample_objective = pd.Series(in_sample_problem.objective.value, index=['in_sample_objective'])
     in_sample_objective.to_csv(os.path.join(results_path, 'in_sample_objective.csv'))
     print(f"in_sample_objective = {in_sample_objective.values}")
+
+    # STEP 2.2: OUT-OF-SAMPLE ANALYSIS.
+
+    # Instantiate problem.
+    # - Utility object for optimization problem definition with CVXPY.
+    out_of_sample_problem = fledge.utils.OptimizationProblem()
+
+    # Define variables.
+    # - Scenario dimension is added by using dicts.
+    out_of_sample_problem.state_vector = dict.fromkeys(out_of_sample_scenarios)
+    out_of_sample_problem.control_vector = dict.fromkeys(out_of_sample_scenarios)
+    out_of_sample_problem.output_vector = dict.fromkeys(out_of_sample_scenarios)
+    out_of_sample_problem.der_thermal_power_vector = dict.fromkeys(out_of_sample_scenarios)
+    out_of_sample_problem.der_active_power_vector = dict.fromkeys(out_of_sample_scenarios)
+    out_of_sample_problem.der_reactive_power_vector = dict.fromkeys(out_of_sample_scenarios)
+    out_of_sample_problem.source_thermal_power_real_time = dict.fromkeys(out_of_sample_scenarios)
+    out_of_sample_problem.source_active_power_real_time = dict.fromkeys(out_of_sample_scenarios)
+
+    for scenario in out_of_sample_scenarios:
+
+        # Flexible DERs: State space vectors.
+        # - CVXPY only allows for 2-dimensional variables. Using dicts below to represent 3rd dimension.
+        out_of_sample_problem.state_vector[scenario] = dict.fromkeys(der_model_set.flexible_der_names)
+        out_of_sample_problem.control_vector[scenario] = dict.fromkeys(der_model_set.flexible_der_names)
+        out_of_sample_problem.output_vector[scenario] = dict.fromkeys(der_model_set.flexible_der_names)
+        for der_name in der_model_set.flexible_der_names:
+            out_of_sample_problem.state_vector[scenario][der_name] = (
+                cp.Variable((
+                    len(der_model_set.flexible_der_models[der_name].timesteps),
+                    len(der_model_set.flexible_der_models[der_name].states)
+                ))
+            )
+            out_of_sample_problem.control_vector[scenario][der_name] = (
+                cp.Variable((
+                    len(der_model_set.flexible_der_models[der_name].timesteps),
+                    len(der_model_set.flexible_der_models[der_name].controls)
+                ))
+            )
+            out_of_sample_problem.output_vector[scenario][der_name] = (
+                cp.Variable((
+                    len(der_model_set.flexible_der_models[der_name].timesteps),
+                    len(der_model_set.flexible_der_models[der_name].outputs)
+                ))
+            )
+
+        # Flexible DERs: Power vectors.
+        out_of_sample_problem.der_thermal_power_vector[scenario] = (
+            cp.Variable((len(timesteps), len(thermal_grid_model.ders)))
+        )
+        out_of_sample_problem.der_active_power_vector[scenario] = (
+            cp.Variable((len(timesteps), len(electric_grid_model.ders)))
+        )
+        out_of_sample_problem.der_reactive_power_vector[scenario] = (
+            cp.Variable((len(timesteps), len(electric_grid_model.ders)))
+        )
+
+        # Source variables: Real time.
+        out_of_sample_problem.source_thermal_power_real_time[scenario] = cp.Variable((len(timesteps), 1), nonpos=True)
+        out_of_sample_problem.source_active_power_real_time[scenario] = cp.Variable((len(timesteps), 1), nonpos=True)
+
+    # Source variables: Day ahead.
+    # - For the out-of-sample, this is a fixed parameter based on the solution of the in-sample problem.
+    out_of_sample_problem.source_thermal_power_day_ahead = (
+        cp.Parameter((len(timesteps), 1), value=in_sample_problem.source_thermal_power_day_ahead.value)
+    )
+    out_of_sample_problem.source_active_power_day_ahead = (
+        cp.Parameter((len(timesteps), 1), value=in_sample_problem.source_active_power_day_ahead.value)
+    )
+
+    # Define constraints.
+
+    for scenario in out_of_sample_scenarios:
+
+        # Flexible DERs.
+        for der_model in der_model_set.flexible_der_models.values():
+
+            # Initial state.
+            out_of_sample_problem.constraints.append(
+                out_of_sample_problem.state_vector[scenario][der_model.der_name][0, :]
+                ==
+                der_model.state_vector_initial.values
+            )
+
+            # State equation.
+            out_of_sample_problem.constraints.append(
+                out_of_sample_problem.state_vector[scenario][der_model.der_name][1:, :]
+                ==
+                cp.transpose(
+                    der_model.state_matrix.values
+                    @ cp.transpose(out_of_sample_problem.state_vector[scenario][der_model.der_name][:-1, :])
+                    + der_model.control_matrix.values
+                    @ cp.transpose(out_of_sample_problem.control_vector[scenario][der_model.der_name][:-1, :])
+                    + der_model.disturbance_matrix.values
+                    @ np.transpose(der_model.disturbance_timeseries.iloc[:-1, :].values)
+                )
+            )
+
+            # Output equation.
+            out_of_sample_problem.constraints.append(
+                out_of_sample_problem.output_vector[scenario][der_model.der_name]
+                ==
+                cp.transpose(
+                    der_model.state_output_matrix.values
+                    @ cp.transpose(out_of_sample_problem.state_vector[scenario][der_model.der_name])
+                    + der_model.control_output_matrix.values
+                    @ cp.transpose(out_of_sample_problem.control_vector[scenario][der_model.der_name])
+                    + der_model.disturbance_output_matrix.values
+                    @ np.transpose(der_model.disturbance_timeseries.values)
+                )
+            )
+
+            # Output limits.
+            out_of_sample_problem.constraints.append(
+                out_of_sample_problem.output_vector[scenario][der_model.der_name]
+                >=
+                der_model.output_minimum_timeseries.values
+            )
+            # For PV power plant, adjust maximum generation limit according to scenario.
+            if der_model.der_type == 'flexible_generator':
+                output_maximum_timeseries = (
+                    pd.concat([
+                        der_model.active_power_nominal * irradiation_out_of_sample.loc[:, scenario].rename('active_power'),
+                        der_model.reactive_power_nominal * irradiation_out_of_sample.loc[:, scenario].rename('reactive_power')
+                    ], axis='columns')
+                )
+                out_of_sample_problem.constraints.append(
+                    out_of_sample_problem.output_vector[scenario][der_model.der_name]
+                    <=
+                    output_maximum_timeseries.replace(np.inf, 1e3).values
+                )
+            else:
+                out_of_sample_problem.constraints.append(
+                    out_of_sample_problem.output_vector[scenario][der_model.der_name]
+                    <=
+                    der_model.output_maximum_timeseries.replace(np.inf, 1e3).values
+                )
+
+            # Power mapping.
+            der_index = int(fledge.utils.get_index(electric_grid_model.ders, der_name=der_model.der_name))
+            out_of_sample_problem.constraints.append(
+                out_of_sample_problem.der_active_power_vector[scenario][:, [der_index]]
+                ==
+                cp.transpose(
+                    der_model.mapping_active_power_by_output.values
+                    @ cp.transpose(out_of_sample_problem.output_vector[scenario][der_model.der_name])
+                )
+            )
+            out_of_sample_problem.constraints.append(
+                out_of_sample_problem.der_reactive_power_vector[scenario][:, [der_index]]
+                ==
+                cp.transpose(
+                    der_model.mapping_reactive_power_by_output.values
+                    @ cp.transpose(out_of_sample_problem.output_vector[scenario][der_model.der_name])
+                )
+            )
+            # - Thermal grid power mapping only for DERs which are connected to the thermal grid.
+            if der_model.der_name in thermal_grid_model.ders.get_level_values('der_name'):
+                der_index = int(fledge.utils.get_index(thermal_grid_model.ders, der_name=der_model.der_name))
+                out_of_sample_problem.constraints.append(
+                    out_of_sample_problem.der_thermal_power_vector[scenario][:, [der_index]]
+                    ==
+                    cp.transpose(
+                        der_model.mapping_thermal_power_by_output.values
+                        @ cp.transpose(out_of_sample_problem.output_vector[scenario][der_model.der_name])
+                    )
+                )
+
+        # Thermal grid.
+
+        # Node head limit.
+        out_of_sample_problem.constraints.append(
+            np.array([node_head_vector_minimum.ravel()])
+            <=
+            cp.transpose(
+                linear_thermal_grid_model.sensitivity_node_head_by_der_power
+                @ cp.transpose(out_of_sample_problem.der_thermal_power_vector[scenario])
+            )
+        )
+
+        # Branch flow limit.
+        out_of_sample_problem.constraints.append(
+            cp.transpose(
+                linear_thermal_grid_model.sensitivity_branch_flow_by_der_power
+                @ cp.transpose(out_of_sample_problem.der_thermal_power_vector[scenario])
+            )
+            <=
+            np.array([branch_flow_vector_maximum.ravel()])
+        )
+
+        # Power balance.
+        out_of_sample_problem.constraints.append(
+            thermal_grid_model.cooling_plant_efficiency ** -1
+            * (
+                out_of_sample_problem.source_thermal_power_day_ahead
+                + out_of_sample_problem.source_thermal_power_real_time[scenario]
+                + cp.sum(-1.0 * (
+                    out_of_sample_problem.der_thermal_power_vector[scenario]
+                ), axis=1, keepdims=True)  # Sum along DERs, i.e. sum for each timestep.
+            )
+            ==
+            cp.transpose(
+                linear_thermal_grid_model.sensitivity_pump_power_by_der_power
+                @ cp.transpose(out_of_sample_problem.der_thermal_power_vector[scenario])
+            )
+        )
+
+        # Electric grid.
+
+        # Voltage limits.
+        out_of_sample_problem.constraints.append(
+            np.array([node_voltage_magnitude_vector_minimum.ravel()])
+            <=
+            np.array([np.abs(linear_electric_grid_model.power_flow_solution.node_voltage_vector.ravel())])
+            + cp.transpose(
+                linear_electric_grid_model.sensitivity_voltage_magnitude_by_der_power_active
+                @ cp.transpose(
+                    out_of_sample_problem.der_active_power_vector[scenario]
+                    - np.array([np.real(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+                + linear_electric_grid_model.sensitivity_voltage_magnitude_by_der_power_reactive
+                @ cp.transpose(
+                    out_of_sample_problem.der_reactive_power_vector[scenario]
+                    - np.array([np.imag(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+            )
+        )
+        out_of_sample_problem.constraints.append(
+            np.array([np.abs(linear_electric_grid_model.power_flow_solution.node_voltage_vector.ravel())])
+            + cp.transpose(
+                linear_electric_grid_model.sensitivity_voltage_magnitude_by_der_power_active
+                @ cp.transpose(
+                    out_of_sample_problem.der_active_power_vector[scenario]
+                    - np.array([np.real(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+                + linear_electric_grid_model.sensitivity_voltage_magnitude_by_der_power_reactive
+                @ cp.transpose(
+                    out_of_sample_problem.der_reactive_power_vector[scenario]
+                    - np.array([np.imag(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+            )
+            <=
+            np.array([node_voltage_magnitude_vector_maximum.ravel()])
+        )
+
+        # Branch flow limits.
+        out_of_sample_problem.constraints.append(
+            np.array([np.abs(linear_electric_grid_model.power_flow_solution.branch_power_vector_1.ravel())])
+            + cp.transpose(
+                linear_electric_grid_model.sensitivity_branch_power_1_magnitude_by_der_power_active
+                @ cp.transpose(
+                    out_of_sample_problem.der_active_power_vector[scenario]
+                    - np.array([np.real(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+                + linear_electric_grid_model.sensitivity_branch_power_1_magnitude_by_der_power_reactive
+                @ cp.transpose(
+                    out_of_sample_problem.der_reactive_power_vector[scenario]
+                    - np.array([np.imag(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+            )
+            <=
+            np.array([branch_power_magnitude_vector_maximum.ravel()])
+        )
+        out_of_sample_problem.constraints.append(
+            np.array([np.abs(linear_electric_grid_model.power_flow_solution.branch_power_vector_2.ravel())])
+            + cp.transpose(
+                linear_electric_grid_model.sensitivity_branch_power_2_magnitude_by_der_power_active
+                @ cp.transpose(
+                    out_of_sample_problem.der_active_power_vector[scenario]
+                    - np.array([np.real(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+                + linear_electric_grid_model.sensitivity_branch_power_2_magnitude_by_der_power_reactive
+                @ cp.transpose(
+                    out_of_sample_problem.der_reactive_power_vector[scenario]
+                    - np.array([np.imag(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+            )
+            <=
+            np.array([branch_power_magnitude_vector_maximum.ravel()])
+        )
+
+        # Power balance.
+        out_of_sample_problem.constraints.append(
+            out_of_sample_problem.source_active_power_day_ahead
+            + out_of_sample_problem.source_active_power_real_time[scenario]
+            + cp.sum(-1.0 * (
+                out_of_sample_problem.der_active_power_vector[scenario]
+            ), axis=1, keepdims=True)  # Sum along DERs, i.e. sum for each timestep.
+            ==
+            np.real(linear_electric_grid_model.power_flow_solution.loss)
+            + cp.transpose(
+                linear_electric_grid_model.sensitivity_loss_active_by_der_power_active
+                @ cp.transpose(
+                    out_of_sample_problem.der_active_power_vector[scenario]
+                    - np.array([np.real(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+                + linear_electric_grid_model.sensitivity_loss_active_by_der_power_reactive
+                @ cp.transpose(
+                    out_of_sample_problem.der_reactive_power_vector[scenario]
+                    - np.array([np.imag(linear_electric_grid_model.power_flow_solution.der_power_vector.ravel())])
+                )
+            )
+        )
+
+    # Define objective.
+
+    # Day-ahead.
+    out_of_sample_problem.objective += (
+        price_data_day_ahead.price_timeseries.loc[:, ('active_power', 'source', 'source')].values.T
+        @ out_of_sample_problem.source_thermal_power_day_ahead
+        * thermal_grid_model.cooling_plant_efficiency ** -1
+    )
+    out_of_sample_problem.objective += (
+        price_data_day_ahead.price_timeseries.loc[:, ('active_power', 'source', 'source')].values.T
+        @ out_of_sample_problem.source_active_power_day_ahead
+    )
+
+    # Real-time.
+    for scenario in out_of_sample_scenarios:
+        out_of_sample_problem.objective += (
+            len(out_of_sample_scenarios) ** -1  # Assuming equal probability.
+            * price_data_real_time.price_timeseries.loc[:, ('active_power', 'source', 'source')].values.T
+            @ out_of_sample_problem.source_thermal_power_real_time[scenario]
+            * thermal_grid_model.cooling_plant_efficiency ** -1
+        )
+        out_of_sample_problem.objective += (
+            len(out_of_sample_scenarios) ** -1  # Assuming equal probability.
+            * price_data_real_time.price_timeseries.loc[:, ('active_power', 'source', 'source')].values.T
+            @ out_of_sample_problem.source_active_power_real_time[scenario]
+        )
+
+    # Solve problem.
+    fledge.utils.log_time('in sample solution')
+    out_of_sample_problem.solve()
+    fledge.utils.log_time('in sample solution')
+
+    # Print objective.
+    out_of_sample_objective = pd.Series(out_of_sample_problem.objective.value, index=['out_of_sample_objective'])
+    out_of_sample_objective.to_csv(os.path.join(results_path, 'out_of_sample_objective.csv'))
+    print(f"out_of_sample_objective = {out_of_sample_objective.values}")
 
     # Plot selected results.
 
